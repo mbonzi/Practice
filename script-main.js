504f1c09db9b7137b05dc4af962aae458273fc2e
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2021-10-13 20:52:29 -0700

    if else

--- script-main.js
+++ script-main.js
@@ -18,3 +18,10 @@ lastNamesArray.reverse();
 for (let i = 0; i < firstNamesArray.length && lastNamesArray.length; i++) {
   console.log(firstNamesArray[i] + " " + lastNamesArray[i]);
 }
+
+
+const age = 15;
+if (age >= 16) {
+    console.log('You are old enough to drive.');
+    }else{
+    console.log('You are not yet old enough to drive.');
