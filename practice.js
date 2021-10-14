927bf8a55a98812427897f12c65c9169eae9f266
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2021-10-13 20:50:03 -0700

    arrays

---
+++ practice.js
@@ -0,0 +1,26 @@
+let myArr = [4,5,6,7, 'end']
+myArr[3];
+console.log(myArr.length)
+myArr.push(6)
+let pos = myArr.indexOf('end')
+let removedItem = myArr.splice(pos, 1)
+
+
+let vegetables = ['cucumber', 'celery', 'carrots', 'zucchini']
+console.log(vegetables)
+let pos = 1
+let n = 2
+let removedItems = vegetables.splice(pos, n)
+console.log(vegetables)
+console.log(removedItems)
+
+/*Code Challenge: JS Review - Loops and Arrays*/
+const firstNamesArray = ['Violet', 'Charlie', 'Veruca', 'Mike', 'Augustus'];
+const lastNamesArray = ['Beauregarde', 'Bucket', 'Salt', 'Teavee', 'Gloop'];
+
+firstNamesArray.reverse();
+lastNamesArray.reverse();
+
+for (let i = 0; i < firstNamesArray.length && lastNamesArray.length; i++) {
+  console.log(firstNamesArray[i] + " " + lastNamesArray[i]);
+}
--- script-main.js
+++ script-main.js
@@ -7,3 +7,14 @@ function changeColor() {
     document.getElementsByTagName("p")[0].textContent=
     document.getElementsByTagName("button")[0].getAttribute("data-text");
   }
+
+  /*Code Challenge: JS Review - Loops and Arrays*/
+const firstNamesArray = ['Violet', 'Charlie', 'Veruca', 'Mike', 'Augustus'];
+const lastNamesArray = ['Beauregarde', 'Bucket', 'Salt', 'Teavee', 'Gloop'];
+
+firstNamesArray.reverse();
+lastNamesArray.reverse();
+
+for (let i = 0; i < firstNamesArray.length && lastNamesArray.length; i++) {
+  console.log(firstNamesArray[i] + " " + lastNamesArray[i]);
+}
