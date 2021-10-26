a9ba2beb56b8da1058fea2640844f90329e359be
Madelyn Bonzi <84095552+mbonzi@users.noreply.github.com>
2021-10-26 16:13:18 -0700

    Create script.js
    Javascript code practice with conversion of Fahrenheit to and from Celsius

---
+++ script.js
@@ -0,0 +1,26 @@
+function toFahrenheit(celsius) {
+  console.log ((celsius * (9/5)) + 32);
+}
+toFahrenheit(15);
+
+toFahrenheit(22);
+
+toFahrenheit(30);
+
+toFahrenheit(35);
+
+toFahrenheit(40);
+
+
+function toCelsius(fahrenheit) {
+  console.log ((fahrenheit - 32)*(5/9));
+}
+toCelsius(104);
+
+toCelsius(95);
+
+toCelsius(86);
+
+toCelsius(71.6);
+
+toCelsius(59);
