6a349f116b3146d8eb861307dfcb94df3998eb68
Madelyn Bonzi <84095552+mbonzi@users.noreply.github.com>
2021-10-27 20:34:01 -0700

    Update script.js
    Arrow function of Celsius to Fahrenheit code

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ function toFahrenheit(celsius) {
   console.log ((celsius * (9/5)) + 32);
 }
 toFahrenheit(15);
-
 toFahrenheit(22);
-
 toFahrenheit(30);
-
 toFahrenheit(35);
-
 toFahrenheit(40);
 
 
@@ -16,11 +12,11 @@ function toCelsius(fahrenheit) {
   console.log ((fahrenheit - 32)*(5/9));
 }
 toCelsius(104);
-
 toCelsius(95);
-
 toCelsius(86);
-
 toCelsius(71.6);
-
 toCelsius(59);
+
+let fromCelsius = c => (c * (9/5) +32);
+console.log(fromCelsius(0));
+console.log(fromCelsius(100));
