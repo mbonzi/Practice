0f4ac2ce311e21aeb888c9882b88d0e517e044e7
Madelyn Bonzi <84095552+mbonzi@users.noreply.github.com>
2021-11-16 21:44:56 -0800

    Update ActiveClock.js

--- ActiveClock.js
+++ ActiveClock.js
@@ -1,22 +1,50 @@
 /* This code is not related to the other files. This is practice utilizing React and JS to create a functional clock that updates each second. */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+export class Clock extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state = { date: new Date() };
   }
+  
   render() {
-    return <div>{this.state.date.toLocaleTimeString()}</div>;
+    
+    return (
+      <div>
+        {this.props.isPrecise
+          ? this.state.date.toISOString()
+          : this.state.date.toLocaleTimeString()}
+      </div>
+    );
   }
+  
+  startInterval() {
+    let delay;
+    if (this.props.isPrecise) {
+      delay = 100;
+    } else {
+      delay = 1000;
+    }
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, delay);
+  }
+
   componentDidMount() {
-    const oneSecond = 1000;
-setInterval(() => {
-  this.setState({ date: new Date() });
-}, oneSecond);
+    this.startInterval();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.isPrecise === prevProps.isPrecise) {
+      return;
+    }
+    clearInterval(this.intervalID);
+    this.startInterval();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
   }
 }
-
-ReactDOM.render(<Clock />, document.getElementById('app'));
